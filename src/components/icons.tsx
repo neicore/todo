d31a41f6101068f69a29ea803c22b137354abe47
neicore <neicoreadams@gmail.com>
2022-09-13 02:33:20 +0300

    :bento: add icons

---
+++ src/components/icons.tsx
@@ -0,0 +1,19 @@
+export const ChevronDown = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      width={20}
+      height={20}
+      strokeWidth={1.5}
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+      />
+    </svg>
+  )
+}
