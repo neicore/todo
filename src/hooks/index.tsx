04449f4b8c94257767d64d7993b8e8995f801c5f
neicore <neicoreadams@gmail.com>
2022-09-14 01:34:11 +0300

    :hook: add useDetectOutsideClick hook

---
+++ src/hooks/index.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from 'react'
+
+export const useDetectOutsideClick = (callback: any) => {
+  const ref = useRef<any>()
+
+  useEffect(() => {
+    const handleClick = (event: any) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        callback()
+      }
+    }
+
+    document.addEventListener('click', handleClick, true)
+
+    return () => {
+      document.removeEventListener('click', handleClick, true)
+    }
+  }, [callback])
+
+  return ref
+}
