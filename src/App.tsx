5b86de8f53ab138d499a3bae43ab0c4e4b6819ef
neicore <neicoreadams@gmail.com>
2022-11-09 21:26:40 +0300

    :sparkles: add todo cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import Greeting from './components/greeting'
 import Header from './components/header'
+import TodoCard from './components/todoCard'
 import Toolbar from './components/toolbar'
+import { todos } from './data'
+import style from './app.module.sass'
 
 function App() {
   return (
@@ -8,6 +11,12 @@ function App() {
       <Header />
       <Greeting />
       <Toolbar />
+
+      <div className={style.todo_container}>
+        {todos.map((todo) => (
+          <TodoCard todo={todo} />
+        ))}
+      </div>
     </>
   )
 }
