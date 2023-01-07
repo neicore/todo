d5eaf5cfe4b99a977d5ad57902d4f31c99f626fd
neicore <neicoreadams@gmail.com>
2023-01-07 18:51:23 +0300

    prevent empty new todo submission

--- src/components/modal/NewTodo.tsx
+++ src/components/modal/NewTodo.tsx
@@ -69,7 +69,7 @@ const NewTodo = () => {
 
           <div className={styles.newTodo_footer_right}>
             <button onClick={handleCancel} className={styles.newTodo_cancel}>
-              close
+              cancel
             </button>
             <button onClick={handleSubmit} className={styles.newTodo_submit}>
               submit
